2851f9b325d2a181b5454ba0ee7041de06924453
Wang, Yuheng (DLSLtd, RAL, SCI) <patrick.wang@diamond.ac.uk>
2023-09-18 11:47:14 +0100

    added comput_corners to plane_mirror class

--- components.py
+++ components.py
@@ -199,7 +199,213 @@ class Plane_Mirror(object):
         The normal vector of the mirror
     orientation : Vector3D
     """
+
+    def __init__(self, voffset=20, hoffset=0, axis_voffset=0, axis_hoffset=0, length=450, width=70, height=50,theta=45, plane=Plane()):
+        self._voffset = voffset
+        self._hoffset = hoffset
+        self._axis_voffset = axis_voffset
+        self._axis_hoffset = axis_hoffset
+        self._length = length
+        self._width = width
+        self._height = height
+        self._plane = plane
+        self._theta = theta
+        _ = self.compute_corners()
+    def __repr__(self):
+        return """Plane_Mirror(voffset={}, 
+        hoffset={}, 
+        axis_voffset={}, 
+        axis_hoffset={}, 
+        length={}, 
+        width={}, 
+        height={}, 
+        plane={})""".format(self.voffset,
+                            self.hoffset, 
+                            self.axis_voffset, 
+                            self.axis_hoffset, 
+                            self.length, 
+                            self.width, 
+                            self.height, 
+                            self.plane)
     
+    @property
+    def voffset(self):
+        return self._voffset
+
+    @voffset.setter
+    def voffset(self, value):
+        self._voffset = value  
+
+    @property
+    def hoffset(self):
+        return self._hoffset
+
+    @hoffset.setter
+    def hoffset(self, value):
+        self._hoffset = value
+
+    @property
+    def axis_voffset(self):
+        return self._axis_voffset
+
+    @axis_voffset.setter
+    def axis_voffset(self, value):
+        self._axis_voffset = value
+
+    @property
+    def axis_hoffset(self):
+        return self._axis_hoffset
+
+    @axis_hoffset.setter
+    def axis_hoffset(self, value):
+        self._axis_hoffset = value
+
+    @property
+    def length(self):
+        return self._length
+
+    @length.setter
+    def length(self, value):
+        if value <= 0:
+            raise ValueError("Mirror length must be greater than zero")
+        self._length = value
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if value <= 0:
+            raise ValueError("Mirror width must be greater than zero")
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        if value <= 0:
+            raise ValueError("Mirror height must be greater than zero")
+        self._height = value
+
+    @property
+    def plane(self):
+        return self._plane
+
+    @plane.setter
+    def plane(self, value):
+        self._plane = value
+
+    def set_position(self, position):
+        self._plane.position = position
+
+    def set_normal(self, normal):
+        self._plane.normal = normal
+
+    def set_orientation(self, orientation):
+        self._plane.orientation = orientation
+
+    def set_dimensions(self, length, width, height):
+        self._length = length
+        self._width = width
+        self._height = height
+
+    def set_offsets(self, voffset, hoffset, axis_voffset, axis_hoffset):
+        self._voffset = voffset
+        self._hoffset = hoffset
+        self._axis_voffset = axis_voffset
+        self._axis_hoffset = axis_hoffset
+
+    def compute_corners(self):
+        """
+        Compute the corners of the mirror in the global coordinate system,
+        in addition to the plane and normal of the mirror.
+    
+
+        Returns
+        -------
+        corners : array_like
+            The corners of the mirror in the global coordinate system
+        """
+        theta_g = 90 - self._theta
+        theta_g = np.deg2rad(theta_g)
+        a = self._hoffset
+        c = self._voffset
+        v = self._axis_voffset
+        h = self._axis_hoffset
+        w = self._width
+        l = self._length
+        d = self._height
+        #Top left front
+
+        tlfz = -((a - c * np.tan(theta_g)) * np.cos(theta_g)) + h
+        tlfy = -(c / np.cos(theta_g) + 
+                 (a - c*np.tan(theta_g)) * np.sin(theta_g)) + v
+        tlfx = -w/2
+        tlf = Point3D(tlfx, tlfy, tlfz)
+
+        #Bottom left front
+        blfz = tlfz + d*np.sin(theta_g)
+        blfy = tlfy - d*np.cos(theta_g)        
+        blfx = -w/2
+        blf = Point3D(blfx, blfy, blfz)
+
+        #Top right front
+        trfz = tlfz
+        trfy = tlfy
+        trfx = w/2
+        trf = Point3D(trfx, trfy, trfz)
+
+        #Bottom right front
+        brfz = blfz
+        brfy = blfy
+        brfx = w/2
+        brf = Point3D(brfx, brfy, brfz)
+
+        #Top left back
+        tlbz = tlfz - l*np.cos(theta_g)
+        tlby = tlfy - l*np.sin(theta_g)
+        tlbx = -w/2
+        tlb = Point3D(tlbx, tlby, tlbz)
+
+        #Bottom left back
+        blbz = tlbz + d*np.sin(theta_g)
+        blby = tlby - d*np.cos(theta_g)
+        blbx = -w/2
+        blb = Point3D(blbx, blby, blbz)
+
+        #Top right back
+        trbz = tlbz
+        trby = tlby
+        trbx = w/2
+        trb = Point3D(trbx, trby, trbz)
+
+        #Bottom right back
+        brbz = blbz
+        brby = blby
+        brbx = w/2
+        brb = Point3D(brbx, brby, brbz)
+
+        self._plane = Plane(tlf, trf, tlb)
+
+        self._corners = np.array([
+            tlf,
+            trf,
+            blf,
+            brf,
+            tlb,
+            trb,
+            blb,
+            brb
+        ])
+
+        return self._corners
+
+    def draw(self, ax):
+        pass
+
 class PGM(object):
     """
     A class for a PGM setup.
@@ -220,3 +426,14 @@ class PGM(object):
         The horizontal offset of the mirror axis in mm
     mirror_leng
     """
+
+    def __init__(self, grating = Grating()):
+        self._grating = grating
+        self._mirror_voffset = 20
+        self._mirror_hoffset = 0
+        self._mirror_axis_voffset = 0
+        self._mirror_axis_hoffset = 0
+        self._mirror_length = 450
+        self._mirror_width = 70
+        self._mirror_height = 50
+        
